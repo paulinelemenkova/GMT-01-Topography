3723fdde81a0e83aa16dd7f68492e22eeaf0b9e8
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-07-31 11:49:52 +0300

    Add files via upload

--- GMT-01-JM-NER.sh
+++ GMT-01-JM-NER.sh
@@ -33,12 +33,12 @@ gmt makecpt -Crelief.cpt -V -T-6857/3206 > myocean.cpt
 ps=Bathymetry_NER.ps
 # Make raster image
 gmt grdimage ner_relief.nc -Cmyocean.cpt -R65/107/-35/21 -JM6i -P -I+a15+ne0.75 -Xc -K > $ps
-#gmt grdimage ner_relief.nc -Cmyocean.cpt -R65/107/-35/21 -JA110/-5/15/6i -P -I+a15+ne0.75 -Xc -K > $ps
 
 # Add grid
 gmt psbasemap -R -J \
     -Bpx10f5a5 -Bpyg10f5a5 -Bsxg5 -Bsyg5 \
     --MAP_TITLE_OFFSET=0.8c \
+    --FONT_TITLE=14p,Helvetica,black \
     --FONT_ANNOT_PRIMARY=7p,Helvetica,black \
     -B+t"Bathymetric map of the Ninety East Ridge region, Indian Ocean" -O -K >> $ps
     
@@ -57,6 +57,110 @@ gmt psbasemap -R -J \
 gmt pscoast -R -J -P \
     -Ia/thinnest,blue -Na -N1/thinner,red -W0.1p -Df -O -K >> $ps
 
+# Texts
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,black+jLB+a-53 -Gwhite@40>> $ps << EOF
+69.5 -26.4 South-East Indian Ridge
+EOF
+
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,black+jLB+a-73 -Gwhite@40>> $ps << EOF
+67.5 2.0 C e n t r a l
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,black+jLB+a-285 -Gwhite@40>> $ps << EOF
+68.5 -10.5 I n d i a n
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,black+jLB+a-53 -Gwhite@40>> $ps << EOF
+67.0 -17.5 R i d g e
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,Helvetica,white+jLB+a-49 >> $ps << EOF
+101.0 0.5 S u m a t r a
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,Helvetica,white+jLB+a-274 >> $ps << EOF
+89.6 -27.0 N     i     n     e     t     y        E     a     s     t        R     i     d     g     e
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,black+jLB+a-285 -Gwhite@40 >> $ps << EOF
+71.0 -9.5 Chagos
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,Helvetica,white+jLB+a-265 >> $ps << EOF
+72.0 -2.0 L a c c a d i v e   R i d g e
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,Helvetica,black+jLB+a-267 -Gwhite@40 >> $ps << EOF
+73.5 1.0 M a l d i v e s
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,Helvetica,white+jLB >> $ps << EOF
+92.0 -12.0 Warton Basin
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,Helvetica,red+jLB -Gwhite@40 >> $ps << EOF
+102.5 0.2 Equator
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,Helvetica,white+jLB >> $ps << EOF
+75.3 -4.5 Central
+75.3 -6.5 Indian
+75.3 -8.5 Basin
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,Helvetica,white+jLB >> $ps << EOF
+82.0 -14.0 Osborn
+82.0 -15.0 Plateau
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f13p,Helvetica,blue+jLB >> $ps << EOF
+86.0 17.5 Bay of
+86.0 15.5 Bengal
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f13p,Helvetica,white+jLB >> $ps << EOF
+65.5 17.5 Arabian
+65.5 15.5 Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,Helvetica,black+jLB -Gwhite@40 >> $ps << EOF
+74.0 17.0 I  n  d  i  a
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f13p,Helvetica,black+jLB -Gwhite@40 >> $ps << EOF
+99.0 16.0 Thailand
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,white+jLB+a-54 >> $ps << EOF
+101.0 5.6 Malaysia
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,black+jLB -Gwhite@40 >> $ps << EOF
+82.2 9.5 Sri
+82.2 8.5 Lanka
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,yellow+jLB+a-274 >> $ps << EOF
+86.0 -4.0 85\232E Ridge
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,white+jLB+a-53 >> $ps << EOF
+95.5 0.5 Sunda Trench
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,Helvetica,black+jLB+a-10 >> $ps << EOF
+92.0 -30.5 Broken Ridge
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,Helvetica,blue+jLB -Gwhite@40 >> $ps << EOF
+70.5 -23.5 Rodrigues
+70.5 -24.5 Triple
+70.5 -25.5 Junction
+EOF
+#
+
 # Add legend
 gmt psscale -Dg65/-38+w15.2c/0.4c+h+o0.0/0i+ml -R -J -Cmyocean.cpt \
     --FONT_LABEL=6p,Helvetica,black \
@@ -66,12 +170,12 @@ gmt psscale -Dg65/-38+w15.2c/0.4c+h+o0.0/0i+ml -R -J -Cmyocean.cpt \
     -I0.2 -By+lm -O -K >> $ps
 
 # Add GMT logo
-gmt logo -Dx6.2/-3.5+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx6.2/-3.3+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y13.0c -N -O \
-    -F+f10p,Palatino-Roman,black+jLB >> $ps << EOF
-3.0 13.5 GEBCO 15 arc sec resolution global terrain model grid
+    -F+f12p,Helvetica,black+jLB >> $ps << EOF
+2.5 13.5 GEBCO 15 arc sec resolution global terrain model grid
 EOF
 
 # Convert to image file using GhostScript
