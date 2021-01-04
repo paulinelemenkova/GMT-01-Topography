00b2d0095118a061d3704e56d83208ef998472e4
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-01-04 11:07:37 +0300

    Add files via upload

--- GMT-01-YE.sh
+++ GMT-01-YE.sh
@@ -18,8 +18,8 @@ gmt set FORMAT_GEO_MAP=dddF \
 gmtdefaults -D > .gmtdefaults
 
 # Extract a subset of ETOPO1m for the study area
-gmt grdcut ETOPO1_Ice_g_gmt4.grd -R42/55/10/20 -Gye_relief.nc
-#gmt grdcut GEBCO_2019.nc -R38/49/29/38 -Gye_relief.nc
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R42/55/10/20 -Gye_relief1.nc
+gmt grdcut GEBCO_2019.nc -R42/55/10/20 -Gye_relief.nc
 gdalinfo -stats ye_relief.nc
 #  Minimum=-5358.000, Maximum=3447.000
 
@@ -35,11 +35,11 @@ gmt psscale -Dg40.5/10.0+w13.0c/0.15i+v+o0.3/0i+ml -R -J -Cmyocean.cpt \
 	--FONT_LABEL=7p,0,black \
     --FONT_ANNOT_PRIMARY=7p,0,black \
     --FONT_TITLE=6p,0,black \
-	-Bg500f50a500+l"Color scale: geo [R=-3637/4124, H=0, C=HSV]" \
+	-Bg500f50a500+l"Color scale: 'globe' [R=-5358/3447, H=0, C=HSV]" \
 	-I0.2 -By+lm -O -K >> $ps
     
 # Add isolines
-gmt grdcontour ye_relief.nc -R -J -C500 -Wthinner,darkbrown -O -K >> $ps
+gmt grdcontour ye_relief1.nc -R -J -C500 -Wthinner,darkbrown -O -K >> $ps
 
 # Add coastlines, borders, rivers
 gmt pscoast -R -J -P \
@@ -51,7 +51,7 @@ gmt psbasemap -R -J \
     --MAP_TITLE_OFFSET=1.0c \
     --FONT_ANNOT_PRIMARY=7p,0,black \
     --FONT_LABEL=7p,25,black \
-    --FONT_TITLE=12p,25,black \
+    --FONT_TITLE=13p,13,black \
     -Bpxg4f1a2 -Bpyg2f1a2 -Bsxg2 -Bsyg1 \
     -B+t"Topographic map of Yemen and Gulf of Aden" -O -K >> $ps
     
@@ -66,58 +66,58 @@ gmt psbasemap -R -J \
 
 # Texts
 gmt pstext -R -J -N -O -K \
--F+jTL+f10p,25,red+jLB+a-345 >> $ps << EOF
-53.0 17.9 O   M   A   N
+-F+jTL+f10p,25,red+jLB+a-340 >> $ps << EOF
+53.0 17.8 O    M    A    N
 EOF
 gmt pstext -R -J -N -O -K \
 -F+jTL+f10p,25,red+jLB+a-340 >> $ps << EOF
 46.1 17.8 S  A  U  D  I     A  R  A  B  I  A
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f9p,25,red+jLB+a-330 -Gwhite@30 >> $ps << EOF
+-F+jTL+f8p,25,red+jLB+a-330 -Gwhite@30 >> $ps << EOF
 42.1 11.5 DJIBOUTI
 EOF
 gmt pstext -R -J -N -O -K \
 -F+jTL+f10p,25,red+jLB+a-350 -Gwhite@60 >> $ps << EOF
-47.0 10.3 S  O  M  A  L  I
+47.0 10.3 S   O   M   A   L   I
 EOF
 gmt pstext -R -J -N -O -K \
 -F+jTL+f16p,25,white+jLB+a-340 >> $ps << EOF
 46.3 15.9 Y        E        M        E        N
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f9p,25,red+jLB+a-50 -Gwhite@30 >> $ps << EOF
+-F+jTL+f8p,25,red+jLB+a-50 -Gwhite@30 >> $ps << EOF
 42.0 13.4 ERITREA
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f8p,25,red+jLB+a-60 -Gwhite@30 >> $ps << EOF
+-F+jTL+f7p,25,red+jLB+a-60 -Gwhite@30 >> $ps << EOF
 42.0 10.9 ETHIOPIA
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f10p,25,blue+jLB+a-335 -Gwhite@60 >> $ps << EOF
-46.5 11.8 G u l f   o f   A d e n
+-F+jTL+f12p,13,blue+jLB+a-340 -Gwhite@50 >> $ps << EOF
+46.5 11.7 G u l f   o f   A d e n
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f11p,13,royalblue4+jLB -Gwhite@40 >> $ps << EOF
+-F+jTL+f11p,13,royalblue4+jLB -Gwhite@50 >> $ps << EOF
 53.4 15.5 Arabian
-53.6 15.2 Sea
+53.7 15.1 Sea
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f11p,13,royalblue4+jLB -Gwhite@40 >> $ps << EOF
+-F+jTL+f11p,13,royalblue4+jLB -Gwhite@50 >> $ps << EOF
 53.7 10.8 Indian
-53.6 10.5 Ocean
+53.6 10.4 Ocean
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f9p,26,blue2+jLB+a-60 >> $ps << EOF
+-F+jTL+f9p,26,blue2+jLB+a-60 -Gwhite@70 >> $ps << EOF
 42.9 13.4 Bab-el-Mandeb
 EOF
 gmt pstext -R -J -N -O -K \
 -F+jTL+f9p,26,chocolate4+jLB+a-340 >> $ps << EOF
 46.1 18.7 A R   R U B'  A L  K H A L I
-46.1 18.4 (Empty Quarter Desert)
+46.5 18.5 (Empty Quarter Desert)
 EOF
 gmt pstext -R -J -N -O -K \
--F+jTL+f10p,26,blue2+jLB+a-77 >> $ps << EOF
+-F+jTL+f10p,26,blue2+jLB+a-82 -Gwhite@70 >> $ps << EOF
 42.2 16.5 R e d   S e a
 EOF
 gmt pstext -R -J -N -O -K \
@@ -125,7 +125,7 @@ gmt pstext -R -J -N -O -K \
 53.5 12.0 Socotra
 EOF
 gmt pstext -R -J -N -O -K \
--F+f12p,13,darkbrown+jLB+a-345 -Gwhite@70 >> $ps << EOF
+-F+f13p,13,darkbrown+jLB+a-345 -Gwhite@70 >> $ps << EOF
 47.7 15.1 H A D H R A M A U T
 EOF
 gmt pstext -R -J -N -O -K \
@@ -133,28 +133,28 @@ gmt pstext -R -J -N -O -K \
 51.0 17.1 Jabal Mahra
 EOF
 gmt pstext -R -J -N -O -K \
--F+f11p,13,khaki1+jLB >> $ps << EOF
+-F+f10p,13,khaki1+jLB >> $ps << EOF
 50.1 16.8 Jabal Bin
 50.1 16.5 Kushayt
 EOF
 gmt pstext -R -J -N -O -K \
--F+f11p,13,khaki1+jLB+a-45 >> $ps << EOF
+-F+f10p,13,khaki1+jLB+a-45 >> $ps << EOF
 44.6 17.3 Ramlat Dahm
 EOF
 gmt pstext -R -J -N -O -K \
--F+f11p,13,khaki1+jLB+a-330 >> $ps << EOF
+-F+f10p,13,khaki1+jLB+a-330 >> $ps << EOF
 46.4 15.3 Ramlat
 46.4 15.0 al-Sab'atayn
 EOF
 gmt pstext -R -J -N -O -K \
--F+f11p,13,khaki1+jLB >> $ps << EOF
-43.7 15.2 Jabal
-43.7 14.9 Haraz
+-F+f9p,13,khaki1+jLB >> $ps << EOF
+43.3 15.2 Jabal
+43.3 14.9 Haraz
 EOF
 # cities
 gmt pstext -R -J -N -O -K \
 -F+f11p,13,black+jLB -Gwhite@30 >> $ps << EOF
-44.2 15.3 Sana'a
+44.2 15.4 Sana'a
 EOF
 gmt psxy -R -J -Ss -W0.5p -Gred -O -K << EOF >> $ps
 44.1 15.2 0.40c
@@ -203,7 +203,7 @@ gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 EOF
 gmt pstext -R -J -N -O -K \
 -F+f9p,13,black+jLB -Gwhite@30 >> $ps << EOF
-43.3 15.5 Amran
+43.3 15.6 Amran
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 43.6 15.4 0.20c
@@ -235,8 +235,8 @@ gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 gmt logo -Dx7.0/-2.0+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
-gmt pstext -R0/10/0/15 -JX10/10 -X0.3c -Y8.0c -N -O \
-    -F+f10p,25,black+jLB >> $ps << EOF
+gmt pstext -R0/10/0/15 -JX10/10 -X0.1c -Y8.0c -N -O \
+    -F+f10p,13,black+jLB >> $ps << EOF
 3.0 9.0 Digital elevation data: SRTM/GEBCO, 15 arc sec resolution grid
 EOF
 
