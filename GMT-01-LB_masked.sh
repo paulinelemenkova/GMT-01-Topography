028e7c3fc676f05fc517dc57d3b73b924a8d95a2
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-01-16 18:23:34 +0300

    Add files via upload

--- GMT-01-LB_masked.sh
+++ GMT-01-LB_masked.sh
@@ -24,84 +24,79 @@ gdalinfo -stats lb_relief.nc
 # Minimum=-2007.000, Maximum=2973.000
 
 # Make color palette
-gmt makecpt -Cgeo.cpt -V -T-2007/2973 > myocean.cpt
+gmt makecpt -Cworld.cpt -V -T-2020/2973 > myocean.cpt
+# elevation etopo1 world elevation
 
 #####################################################################
 # create mask of vector layer from the DCW of country's polygon
-gmt pscoast -R34.7/36.7/32.8/34.8 -JM6.5i -Dh -M -ELB > Lebanon.txt
+#gmt pscoast -R34.7/36.7/32.8/34.8 -JM6.5i -Dh -M -ELB > Lebanon.txt
+gmt pscoast -Dh -M -ELB > Lebanon.txt
 #####################################################################
 
 ps=Topo_LB.ps
 # Make background transparent image
-gmt grdimage lb_relief.nc -Cmyocean.cpt -R34.7/36.7/32.8/34.8 -JM6.5i -I+a15+ne0.75 -t35 -Xc -P -K > $ps
+gmt grdimage lb_relief.nc -Cmyocean.cpt -R34.7/36.7/32.8/34.8 -JM6.5i -I+a15+ne0.75 -t60 -Xc -P -K > $ps
     
 # Add isolines
-gmt grdcontour lb_relief.nc -R -J -C500 -Wthinner,darkbrown -O -K >> $ps
+gmt grdcontour lb_relief.nc -R -J -C100 -A200 -Wthinner,darkbrown -O -K >> $ps
 
 # Add coastlines, borders, rivers
-gmt pscoast -R -J -P \
+#gmt pscoast -R -J -P \
     -Ia/thinner,blue -Na -N1/thickest,white -W0.1p -Df -O -K >> $ps
+gmt pscoast -R -J -P \
+    -Ia/thinner,blue -Na -W0.1p -Df -O -K >> $ps
 
 #####################################################################
 # CLIPPING
-# 1. Start: clip the map by mask to only include Jordan
+# 1. Start: clip the map by mask to only include country
 gmt psclip -JM -R Lebanon.txt -O -K >> $ps
 
 # 2. create map within mask
 # Add raster image
 gmt grdimage lb_relief.nc -Cmyocean.cpt -R34.7/36.7/32.8/34.8 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
 # Add isolines
-gmt grdcontour lb_relief.nc -R -J -C500 -Wthinnest,navajowhite1 -O -K >> $ps
+gmt grdcontour lb_relief.nc -R -J -C500 -Wthinner,darkbrown -O -K >> $ps
 # Add coastlines, borders, rivers
-gmt pscoast -R -J -P \
-    -Ia/thinner,blue -Na -N1/thickest,red -W0.1p -Df -O -K >> $ps
+gmt pscoast -R -J \
+    -Ia/thinner,blue -Na -N1/2pt,red -W0.1p -Df -O -K >> $ps
 
 # 3: Undo the clipping
 gmt psclip -C -O -K >> $ps
 #####################################################################
 
 # Add color legend
-gmt psscale -Dg27.5/-13.0+w17.7c/0.15i+v+o0.3/0i+ml -R -J -Cmyocean.cpt \
+gmt psscale -Dg34.7/32.68+w17.0c/0.15i+h+o0.3/0i+ml -R -J -Cmyocean.cpt \
     --FONT_LABEL=7p,0,black \
     --FONT_ANNOT_PRIMARY=7p,0,black \
     --FONT_TITLE=6p,0,black \
-    -Bg500f50a500+l"Color scale: 'geo' [R=-5358/3447, H=0, C=HSV]" \
+    -Bg500f50a500+l"Color scale: 'world' [R=-5358/3447, H=0, C=HSV]" \
     -I0.2 -By+lm -O -K >> $ps
     
 # Add grid
 gmt psbasemap -R -J \
-    --MAP_FRAME_AXES=wESN \
+    --MAP_FRAME_AXES=WEsN \
     --MAP_TITLE_OFFSET=0.8c \
     --FONT_ANNOT_PRIMARY=7p,0,black \
     --FONT_LABEL=7p,25,black \
     --FONT_TITLE=13p,13,black \
-    -Bpxg4f1a2 -Bpyg2f1a2 -Bsxg2 -Bsyg1 \
+    -Bpxg1f0.1a1 -Bpyg1f0.1a1 -Bsxg1 -Bsyg1 \
     -B+t"Topographic map of Lebanon" -O -K >> $ps
     
 # Add scalebar, directional rose
 gmt psbasemap -R -J \
-    --FONT=7p,0,black \
-    --FONT_ANNOT_PRIMARY=6p,0,black \
+    --FONT_ANNOT_PRIMARY=8p,0,black \
     --MAP_TITLE_OFFSET=0.1c \
     --MAP_ANNOT_OFFSET=0.1c \
-    -Lx14.5c/-1.3c+c50+w200k+l"Mercator projection. Scale (km)"+f \
-    -UBL/-15p/-38p -O -K >> $ps
+    -Lx14.5c/-2.4c+c10+w50k+l"Mercator projection. Scale (km)"+f \
+    -UBL/-10p/-70p -O -K >> $ps
 
 # Texts
 
-# insert map
-# Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
-gmt psbasemap -R -J -O -K -DjTR+w3.2c+o-0.2c/-0.2c+stmp >> $ps
-read x0 y0 w h < tmp
-gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG35.8/34.0N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -ELB+gred -Sdodgerblue -O -K -X$x0 -Y$y0 >> $ps
-#gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
-gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
-
 # Add GMT logo
-gmt logo -Dx7.0/-2.0+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx7.0/-3.0+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
-gmt pstext -R0/10/0/15 -JX10/10 -X0.1c -Y12.5c -N -O \
+gmt pstext -R0/10/0/15 -JX10/10 -X0.1c -Y14.4c -N -O \
     -F+f10p,13,black+jLB >> $ps << EOF
 3.0 9.0 Digital elevation data: SRTM/GEBCO, 15 arc sec resolution grid
 EOF
