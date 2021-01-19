7f3631526187422a72c6221ab87eaeaea8c14ad6
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-01-19 18:47:55 +0300

    Add files via upload

---
+++ GMT-01-ET_masked.sh
@@ -0,0 +1,156 @@
+#!/bin/sh
+# Purpose: shaded relief grid raster map from the GEBCO 15 arc sec global data set (here: Ethiopia)
+# GMT modules: gmtset, gmtdefaults, grdcut, makecpt, grdimage, psscale, grdcontour, psbasemap, gmtlogo, psconvert
+
+# GMT set up
+gmt set FORMAT_GEO_MAP=dddF \
+    MAP_FRAME_PEN=dimgray \
+    MAP_FRAME_WIDTH=0.1c \
+    MAP_TITLE_OFFSET=1c \
+    MAP_ANNOT_OFFSET=0.1c \
+    MAP_TICK_PEN_PRIMARY=thinner,dimgray \
+    MAP_GRID_PEN_PRIMARY=thin,white \
+    MAP_GRID_PEN_SECONDARY=thinnest,white \
+    FONT_TITLE=12p,Palatino-Roman,black \
+    FONT_ANNOT_PRIMARY=7p,0,dimgray \
+    FONT_LABEL=7p,0,dimgray \
+# Overwrite defaults of GMT
+gmtdefaults -D > .gmtdefaults
+
+# Extract a subset of ETOPO1m for the study area
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R33/48/3/15 -Get_relief.nc
+#gmt grdcut GEBCO_2019.nc -R33/48/3/15 -Get_relief.nc
+gdalinfo -stats et_relief.nc
+# Minimum=-3481.000, Maximum=4326.000
+
+# Make color palette
+gmt makecpt -Cturbo.cpt -V -T-3481/4326 > myocean.cpt
+# elevation etopo1 world elevation dem1 dem2 dem3
+
+#####################################################################
+# create mask of vector layer from the DCW of country's polygon
+gmt pscoast -R33/48/3/15 -JM6.5i -Dh -M -EET > Ethiopia.txt
+#gmt pscoast -Dh -M -ELB > Ethiopia.txt
+#####################################################################
+
+ps=Topo_ET.ps
+# Make background transparent image
+gmt grdimage et_relief.nc -Cmyocean.cpt -R33/48/3/15 -JM6.5i -I+a15+ne0.75 -t60 -Xc -P -K > $ps
+    
+# Add isolines
+gmt grdcontour et_relief.nc -R -J -C500 -A1000 -Wthinner,darkbrown -O -K >> $ps
+
+# Add coastlines, borders, rivers
+gmt pscoast -R -J -P \
+    -Ia/thinner,blue -Na -W0.1p -Df -O -K >> $ps
+    
+#####################################################################
+# CLIPPING
+# 1. Start: clip the map by mask to only include country
+#gmt psclip -JM -R Ethiopia.txt -O -K >> $ps
+
+gmt psclip -R33/48/3/15 -JM6.5i Ethiopia.txt -O -K >> $ps
+
+# 2. create map within mask
+# Add raster image
+gmt grdimage et_relief.nc -Cmyocean.cpt -R33/48/3/15 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
+# Add isolines
+gmt grdcontour et_relief.nc -R -J -C500 -Wthinnest,darkbrown -O -K >> $ps
+# Add coastlines, borders, rivers
+gmt pscoast -R -J \
+    -Ia/thinner,blue -Na -N1/thickest,red -W0.1p -Df -O -K >> $ps
+#gmt pscoast -R -J \
+    -Ia/thinner,blue -Na -W0.1p -Df -O -K >> $ps
+
+# 3: Undo the clipping
+gmt psclip -C -O -K >> $ps
+#####################################################################
+    
+# Add color legend
+gmt psscale -Dg31.5/3+w13.3c/0.15i+v+o0.0/0i+ml -R -J -Cmyocean.cpt \
+    --FONT_LABEL=8p,0,black \
+    --FONT_ANNOT_PRIMARY=7p,0,black \
+    --FONT_TITLE=6p,0,black \
+    -Bg500f50a500+l"Color scale: 'turbo' colors for bathymetry/topography [R=-3481/4326, H=0, C=HSV]" \
+    -I0.2 -By+lm -O -K >> $ps
+    
+# Add grid
+gmt psbasemap -R -J \
+    --MAP_FRAME_AXES=wESN \
+    --FORMAT_GEO_MAP=ddd:mm:ss \
+    --MAP_TITLE_OFFSET=0.8c \
+    --FONT_ANNOT_PRIMARY=7p,0,black \
+    --FONT_LABEL=7p,25,black \
+    --FONT_TITLE=16p,13,black \
+    -Bpxg2f0.5a2 -Bpyg2f2a2 -Bsxg1 -Bsyg1 \
+    -B+t"Topographic map of Ethiopia" -O -K >> $ps
+    
+# Add scalebar, directional rose
+gmt psbasemap -R -J \
+    --FONT_LABEL=8p,0,black \
+    --FONT_ANNOT_PRIMARY=8p,0,black \
+    --MAP_TITLE_OFFSET=0.1c \
+    --MAP_ANNOT_OFFSET=0.1c \
+    -Lx14.5c/-1.2c+c10+w300k+l"Mercator projection. Scale (km)"+f \
+    -UBL/-10p/-40p -O -K >> $ps
+
+# Texts
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,0,royalblue4+jLB >> $ps << EOF
+40.2 15.2 Red
+40.2 14.8 Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,0,royalblue4+jLB >> $ps << EOF
+46.0 13.0 Gulf of
+46.3 12.7 Aden
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,0,royalblue4+jLB+a-30 >> $ps << EOF
+48.3 29.1 Bab al Mandab
+EOF
+# COUNTRIES
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,25,firebrick3+jLB -Gwhite@30 -Wthinnest >> $ps << EOF
+34.8 14.8 SUDAN
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,25,firebrick3+jLB -Gwhite@30 -Wthinnest >> $ps << EOF
+46.7 36.5 SOMALIA
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,25,firebrick3+jLB -Gwhite@30 -Wthinnest >> $ps << EOF
+36.1 3.8 KENYA
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,25,firebrick3+jLB -Gwhite@30 -Wthinnest >> $ps << EOF
+44.8 14.7 YEMEN
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,25,firebrick3+jLB -Gwhite@30 -Wthinnest >> $ps << EOF
+37.0 15.1 ERITREA
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,25,firebrick3+jLB -Gwhite@30 -Wthinnest >> $ps << EOF
+40.2 12.5 DJIBOUTI
+EOF
+
+# insert map
+# Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
+gmt psbasemap -R -J -O -K -DjTL+w3.2c+o-0.2c/-0.2c+stmp >> $ps
+read x0 y0 w h < tmp
+gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG35.0/-6.0N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -EET+gred -Sdodgerblue -O -K -X$x0 -Y$y0 >> $ps
+#gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
+gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
+
+# Add GMT logo
+gmt logo -Dx7.0/-2.0+o0.1i/0.1i+w2c -O -K >> $ps
+
+# Add subtitle
+gmt pstext -R0/10/0/15 -JX10/10 -X0.1c -Y8.0c -N -O \
+    -F+f10p,13,black+jLB >> $ps << EOF
+3.0 9.0 Digital elevation data: SRTM/GEBCO, 15 arc sec resolution grid
+EOF
+
+# Convert to image file using GhostScript
+gmt psconvert Topo_ET.ps -A0.5c -E720 -Tj -Z
