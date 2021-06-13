51b73a4067dc34c82f317434dc5dd79409082ed8
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-06-13 08:25:30 +0300

    Add files via upload

---
+++ GMT-01-SE_JT.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+# Purpose: shaded relief grid raster map from the GEBCO 15 arc sec global data set (here: Uganda)
+# GMT modules: gmtset, gmtdefaults, grdcut, makecpt, grdimage, psscale, grdcontour, psbasemap, gmtlogo, psconvert
+# http://soliton.vm.bytemark.co.uk/pub/cpt-city/njgs/index.html
+
+# GMT set up
+gmt set FORMAT_GEO_MAP=dddF \
+    MAP_FRAME_PEN=dimgray \
+    MAP_FRAME_WIDTH=0.1c \
+    MAP_TITLE_OFFSET=1c \
+    MAP_ANNOT_OFFSET=0.1c \
+    MAP_TICK_PEN_PRIMARY=thinner,dimgray \
+    MAP_GRID_PEN_PRIMARY=thin,white \
+    MAP_GRID_PEN_SECONDARY=thinnest,white \
+    FONT_TITLE=12p,Palatino-Roman,black \
+    FONT_ANNOT_PRIMARY=7p,0,dimgray \
+    FONT_LABEL=7p,0,dimgray \
+# Overwrite defaults of GMT
+gmtdefaults -D > .gmtdefaults
+
+# Extract a subset of ETOPO1m for the study area
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R3/26/54/70 -Gse_relief.nc
+#gmt grdcut GEBCO_2019.nc -R29/35/-1.5/4.3 -Gse_relief.nc
+gdalinfo -stats se_relief.nc
+#  Minimum=-3067.000, Maximum=2225.000, Mean=120.486, StdDev=446.592
+
+#####################################################################
+# create mask of vector layer from the DCW of country's polygon
+gmt pscoast -R10/54/26/69.5r -JT17/4.5i -Dh -M -ESE > Sweden.txt
+#####################################################################
+
+# Make color palette
+gmt makecpt -Cgeo -V -T-3067/2225 > pauline.cpt
+# elevation etopo1 world elevation dem1 dem2 dem3 globe geo srtm turbo terra earth relief costa-rica
+
+ps=Topo_SE.ps
+# Make background transparent image
+gmt grdimage se_relief.nc -Cpauline.cpt -R10/54/26/69.5r -JT17/4.5i -I+a15+ne0.75 -t50 -Xc -P -K > $ps
+    
+# Add isolines
+gmt grdcontour se_relief.nc -R -J -C1000 -A1000+f7p,26,darkbrown -Wthinner,darkbrown -O -K >> $ps
+
+# Add coastlines, borders, rivers
+gmt pscoast -R -J \
+    -Ia/thinner,blue -Na -N1/thicker,red -W0.1p -Df -O -K >> $ps
+    
+#####################################################################
+# CLIPPING
+# 1. Start: clip the map by mask to only include country
+gmt psclip -R10/54/26/69.5r -JT17/4.5i Sweden.txt -O -K >> $ps
+
+# 2. create map within mask
+# Add raster image
+gmt grdimage se_relief.nc -Cpauline.cpt -R10/54/26/69.5r -JT17/4.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
+# Add isolines
+gmt grdcontour se_relief.nc -R -J -C250 -Wthinnest,darkbrown -O -K >> $ps
+# Add coastlines, borders, rivers
+gmt pscoast -R -J \
+    -Ia/thinner,blue -Na -N1/thicker,tomato -W0.1p -Df -O -K >> $ps
+
+# 3: Undo the clipping
+gmt psclip -C -O -K >> $ps
+#####################################################################
+    
+# Add color legend
+gmt psscale -Dg10.0/53.2+w11.5c/0.15i+h+o0.3/0i+ml+e -R -J -Cpauline.cpt \
+    --FONT_LABEL=8p,0,black \
+    --FONT_ANNOT_PRIMARY=7p,0,black \
+    --FONT_TITLE=6p,0,black \
+    -Bg500f50a500+l"Colormap: 'geo' scheme for topography. [R=-3067/2225, H, C=RGB]" \
+    -I0.2 -By+lm -O -K >> $ps
+    
+# Add grid
+gmt psbasemap -R -J \
+    --MAP_FRAME_AXES=WEsN \
+    --FORMAT_GEO_MAP=ddd:mm:ssF \
+    -Bpxg4f2a2 -Bpyg2f4a2 -Bsxg2 -Bsyg2 \
+    --MAP_TITLE_OFFSET=0.8c \
+    --FONT_ANNOT_PRIMARY=8p,0,black \
+    --FONT_LABEL=8p,25,black \
+    --FONT_TITLE=16p,25,black \
+    -B+t"Topographic map of Sweden" -O -K >> $ps
+    
+# Add scalebar, directional rose
+gmt psbasemap -R -J \
+    --FONT_LABEL=8p,0,black \
+    --FONT_ANNOT_PRIMARY=8p,0,black \
+    --MAP_TITLE_OFFSET=0.1c \
+    --MAP_ANNOT_OFFSET=0.1c \
+    -Lx9.5c/-2.5c+c10+w300k+l"Transverse Mercator Prj. Scale (km)"+f \
+    -UBL/0p/-70p -O -K >> $ps
+
+# Step-7. Study area
+gmt psbasemap -R -J \
+    -D144/40.2/162/51r -F+pthin,red \
+    -O -K >> $ps
+# Step-8. Study area label annotation
+gmt psxy -R -J -Wthin -O -K \
+    -Sqn1:+f12p,Times-Roman,red+l"Study Area"+c5p+pthick,red+o << EOF >> $ps
+144.2 41.1
+160.8 41.5
+EOF
+
+# Texts
+
+# Add GMT logo
+gmt logo -Dx4.5/-3.1+o0.1i/0.1i+w2c -O -K >> $ps
+
+# Add subtitle
+gmt pstext -R0/10/0/15 -JX10/10 -X0.2c -Y19.2c -N -O \
+    -F+f10p,0,black+jLB >> $ps << EOF
+0.0 9.0 Digital elevation data: GEBCO grid, 15 arc sec (ca. 450 m) resolution
+EOF
+
+# Convert to image file using GhostScript
+gmt psconvert Topo_SE.ps -A0.5c -E720 -Tj -Z
