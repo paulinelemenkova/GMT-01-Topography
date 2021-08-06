3c5e5862ee2c5c3ebb69575baf86eab5057382a9
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-08-06 12:41:15 +0300

    Add files via upload

--- GMT-01-MX.sh
+++ GMT-01-MX.sh
@@ -69,11 +69,11 @@ gmt psclip -C -O -K >> $ps
 #####################################################################
 
 # Add color barlegend
-gmt psscale -Dg240/11.7+w15.3c/0.4c+h+o0.0/0i+ml -R -J -Cpauline.cpt \
+gmt psscale -Dg240/11.7+w15.3c/0.4c+h+o0.0/0i+ml+e -R -J -Cpauline.cpt \
     --FONT_LABEL=8p,Helvetica,black \
     --MAP_LABEL_OFFSET=0.1c \
     --FONT_ANNOT_PRIMARY=6p,0,black \
-    -Baf+l"Color scale 'geo': global bathymetry/topography relief [R=-6857/3206, H=0, C=RGB]" \
+    -Bg500a1000f100+l"Color scale 'geo': global bathymetry/topography relief [R=-6857/3206, H=0, C=RGB]" \
     -I0.2 -By+lm -O -K >> $ps
     
 # Add grid
@@ -95,12 +95,157 @@ gmt psbasemap -R -J \
     -UBL/-5p/-60p -O -K >> $ps
     
 # Texts
+# Cities
+gmt pstext -R -J -N -O -K \
+-F+f10p,21,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+258.0 19.73 Mexico City
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gred -O -K << EOF >> $ps
+260.87 19.43 0.25c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,21,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+257.3 16.0 Acapulco
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+260.12 16.86 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,21,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+253.9 23.73 Chihuahua
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+253.9 23.43 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,21,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+259.70 25.37 Monterrey
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+259.70 25.67 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,21,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+257.3 23.6 Zacatecas
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+257.3 23.3 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,0,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+254.34 26.63 Hidalgo
+254.34 26.33 del Parral
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+254.34 26.93 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,0,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+259.02 22.45 San Luis Potosí
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+259.02 22.15 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,0,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+256.55 25.24 Torreón
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+256.55 25.54 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,0,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+258.32 21.42 León
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+258.32 21.12 0.20c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f9p,0,black+jLB+a-0 -Gwhite@60 >> $ps << EOF
+261.82 18.73 Puebla
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+261.82 19.03 0.20c
+EOF
+
+# countries -R240/275/14/33
+gmt pstext -R -J -N -O -K \
+-F+jTL+f13p,25,black+jLB >> $ps << EOF
+261.0 31.0 U. S. A.
+EOF
+#gmt pstext -R -J -N -O -K \
+#-F+jTL+f14p,25,black+jLB -Gwhite@50 >> $ps << EOF
+#255.7 21.5 M E X I C O
+#EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,21,black+jLB -Gwhite@60 >> $ps << EOF
+268.2 15.0 Guatemala
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,21,black+jLB -Gwhite@60 >> $ps << EOF
+271.0 17.0 Belize
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,21,black+jLB -Gwhite@60 >> $ps << EOF
+271.4 14.5 Honduras
+EOF
+
+# water
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,blue1+jLB >> $ps << EOF
+266 25 Gulf of Mexico
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB >> $ps << EOF
+264.2 20.0 Bay of
+264.2 19.3 Campeche
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB >> $ps << EOF
+264.2 15.1 Gulf of
+263.0 14.3 Tehuantepec
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,blue1+jLB >> $ps << EOF
+241.1 21.0 P A C I F I C   O C E A N
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,blue1+jLB+a-55 >> $ps << EOF
+246.0 30.2 Gulf of California
+EOF
+
+# mountains
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,23,black+jLB+a-25 -Gwhite@50 >> $ps << EOF
+258.0 18.4 Sierra Madre del Sur
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,23,black+jLB+a-70 -Gwhite@50 >> $ps << EOF
+257.5 29.0 Sierra Madre Oriental
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,23,black+jLB+a-60 -Gwhite@50 >> $ps << EOF
+251.0 29.5 Sierra Madre Occidental
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,23,gold1+jLB+a-315 >> $ps << EOF
+270.0 18.3 Yucatán
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,23,black+jLB+a-60 -Gwhite@60 >> $ps << EOF
+243.8 31.9 Baja California
+EOF
+# rivers -R285/328/-35/6
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-50 >> $ps << EOF
+258.5 29.5 Rio Grande
+EOF
 
 # insert map
 # Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
 gmt psbasemap -R -J -O -K -DjBL+w2.7c+stmp >> $ps
 read x0 y0 w h < tmp
-gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG270/15N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ESA+gpeachpuff -EMX+gyellow -Sskyblue1 -O -K -X$x0 -Y$y0 >> $ps
+gmt pscoast --MAP_GRID_PEN_PRIMARY=thinner,white -Rg -JG270/15N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ESA+gpeachpuff -EMX+gyellow -Sskyblue1 -O -K -X$x0 -Y$y0 >> $ps
 #gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
