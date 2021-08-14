71364bc2a08b8f9118b3ead1450e47e4dce6400a
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2021-08-14 16:02:08 +0300

    Add files via upload

--- GMT-01-PY.sh
+++ GMT-01-PY.sh
@@ -21,8 +21,8 @@ chsh -s /bin/bash
 
 chsh -s /bin/zsh
 
-gmt grdcut GEBCO_2019.nc -R297/306/-28/-19 -GParaguay_relief.nc
-gmt grdcut ETOPO1_Ice_g_gmt4.grd -R297/306/-28/-19 -GParaguay_relief1.nc
+#gmt grdcut GEBCO_2019.nc -R297/306/-28/-19 -GParaguay_relief.nc
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R297/306/-28/-19 -GParaguay_relief.nc
 
 gdalinfo Paraguay_relief.nc -stats
 # Minimum=51.000, Maximum=882.000, Mean=202.711, StdDev=125.633
@@ -43,7 +43,7 @@ ps=Topography_Paraguay.ps
 gmt grdimage Paraguay_relief.nc -Cpauline.cpt -R297/306/-28/-19 -JM6i -P -I+a15+ne0.75 -t50 -Xc -K > $ps
     
 # Add isolines
-gmt grdcontour Paraguay_relief1.nc -R -J -C50 -W0.1p -O -K >> $ps
+gmt grdcontour Paraguay_relief.nc -R -J -C50 -W0.1p -O -K >> $ps
 
 # Add coastlines, borders, rivers
 gmt pscoast -R -J -P \
@@ -73,7 +73,7 @@ gmt psscale -Dg297/-28.6+w15.2c/0.4c+h+o0.0/0i+ml -R -J -Cpauline.cpt \
     --FONT_LABEL=8p,Helvetica,black \
     --MAP_LABEL_OFFSET=0.1c \
     --FONT_ANNOT_PRIMARY=6p,Helvetica,black \
-    -Baf+l"Color scale 'geo': global bathymetry/topography relief [R=-6857/3206, H=0, C=RGB]" \
+    -Ba100g100f10+l"Color scale 'geo': global bathymetry/topography relief [R=-6857/3206, H=0, C=RGB]" \
     -I0.2 -By+lm -O -K >> $ps
     
 # Add grid
@@ -82,8 +82,8 @@ gmt psbasemap -R -J \
     --FORMAT_GEO_MAP=ddd:mm:ssF \
     -Bpx1f1a2 -Bpyg1f1a2 -Bsxg1 -Bsyg1 \
     --MAP_TITLE_OFFSET=1.2c \
-    --FONT_TITLE=14p,Helvetica,black \
-    --FONT_ANNOT_PRIMARY=7p,Helvetica,black \
+    --FONT_TITLE=14p,0,black \
+    --FONT_ANNOT_PRIMARY=8p,0,black \
     -B+t"Topographic map of Paraguay" -O -K >> $ps
     
 # Add scalebar, directional rose
@@ -95,17 +95,145 @@ gmt psbasemap -R -J \
     -UBL/-5p/-65p -O -K >> $ps
     
 # Texts
+# countries -R297/306/-28/-19
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,19,black+jLB -Gwhite@50 >> $ps << EOF
+297.3 -19.3 B O L I V I A
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,25,black+jLB -Gwhite@50 >> $ps << EOF
+303.5 -21.5 B R A Z I L
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,25,black+jLB >> $ps << EOF
+299.2 -26.5 A R G E N T I N A
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f16p,27,white+jLB >> $ps << EOF
+299.5 -22.9 P A R A G U A Y
+EOF
+# geography
+gmt pstext -R -J -N -O -K \
+-F+jTL+f14p,22,BURLYWOOD4+jLB+a-310 >> $ps << EOF
+299.0 -25.0 G R A N   C H A C O
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f13p,22,khaki1+jLB >> $ps << EOF
+298.1 -21.5 CHACO BOREAL
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,22,BURLYWOOD4+jLB >> $ps << EOF
+299.8 -25.0 C H A C O
+299.6 -25.3 C E N T R A L
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,22,BURLYWOOD4+jLB >> $ps << EOF
+299.2 -25.9 C H A C O  A S T R A L
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,17,yellow+jLB -GSADDLEBROWN@80 >> $ps << EOF
+303.0 -24.8 O R I E N T A L
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,22,white+jLB -GSADDLEBROWN@50 >> $ps << EOF
+304.7 -25.6 PARANA
+304.7 -25.8 PLATEAU
+EOF
+# Parks
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,14,darkgreen+jLB >> $ps << EOF
+298.0 -22.2 Teniente Encisco
+298.0 -22.5 National Park
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,14,darkgreen+jLB >> $ps << EOF
+299.5 -20.2 Defensores
+299.8 -20.4 del Chaco
+299.5 -20.6 National Park
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,13,white+jLB -GSADDLEBROWN@50 >> $ps << EOF
+302.5 -22.5 Cerro Corá
+302.5 -22.7 National Park
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f11p,14,darkgreen+jLB >> $ps << EOF
+300.3 -23.9 Tinfunque
+300.3 -24.1 National Park
+EOF
+
+# mountains
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,23,white+jLB+a-70 -GSADDLEBROWN@50 >> $ps << EOF
+304.1 -22.5 Cordillera
+303.9 -22.5 de Amambay
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,23,white+jLB -GSADDLEBROWN@50 >> $ps << EOF
+304.5 -24.2 Cordillera
+304.5 -24.35 de Mbaracay
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,23,white+jLB+a-320 -GSADDLEBROWN@50 >> $ps << EOF
+304.1 -26.7 Cordillera de
+304.3 -26.8 San Rafael
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,23,white+jLB+a-70 -GSADDLEBROWN@50 >> $ps << EOF
+304.1 -25.0 Cordillera de
+304.0 -25.2 Caaguazú
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,23,white+jLB -GSADDLEBROWN@50 >> $ps << EOF
+298.8 -20.2 Léon Hill
+EOF
+
+# rivers
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Verde
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Negro
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Paraguay
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Paraná
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Aquidaban
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Ypané
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Paraguay
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,26,blue1+jLB+a-55 >> $ps << EOF
+258.6 30.0 Lake
+258.6 30.0 Trinidad
+EOF
+# cities
 
 # insert map
 # Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
-gmt psbasemap -R -J -O -K -DjTR+w3.7c+stmp >> $ps
+gmt psbasemap -R -J -O -K -DjBL+w3.7c+stmp >> $ps
 read x0 y0 w h < tmp
-gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG302/24S/$w -Da -Gbrown -A5000 -Bg -Wfaint -ESA+gpeachpuff -EPY+gyellow -Slightskyblue1 -O -K -X$x0 -Y$y0 >> $ps
+gmt pscoast --MAP_GRID_PEN_PRIMARY=thinnest,white -Rg -JG302/24S/$w -Da -Gbrown -A5000 -Bg -Wfaint -ESA+gpeachpuff -EPY+gyellow -Slightskyblue1 -O -K -X$x0 -Y$y0 >> $ps
 #gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
 # Add GMT logo
-gmt logo -Dx6.2/-3.0+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx6.5/-3.0+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y8.5c -N -O \
