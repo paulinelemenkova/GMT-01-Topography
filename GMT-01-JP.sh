1654077bc958f3d942ea54ec5e260f9d618c4055
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2022-12-24 12:13:34 +0100

    Add files via upload

--- GMT-01-JP.sh
+++ GMT-01-JP.sh
@@ -81,8 +81,8 @@ gmt psbasemap -R -J \
     
 # Texts
 gmt pstext -R -J -N -O -K \
--F+f12p,0,white+jLB >> $ps << EOF
-133 41 SEA OF JAPAN
+-F+f11p,0,white+jLB >> $ps << EOF
+133 41 S E A  O F  J A P A N
 144.05 37.5 P A C I F I C  O C E A N
 EOF
 gmt pstext -R -J -N -O -K \
@@ -97,83 +97,81 @@ EOF
 
 # -R128/150/30/46
 gmt pstext -R -J -N -O -K \
--F+f14p,22,black+jLB -Gwhite@50 >> $ps << EOF
-139.69 35.79 Tokyo
-EOF
-gmt psxy -R -J -Ss -W0.5p -Gred -O -K << EOF >> $ps
-139.69 35.69 0.30c
-EOF
-gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-139.63 35.30 Yokohama
+-F+f11p,21,black+jLB -Gwhite@50 >> $ps << EOF
+139.53 35.0 Yokohama
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 139.63 35.44 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-135.60 34.59 Osaka
+-F+f12p,21,black+jLB -Gwhite@50 >> $ps << EOF
+135.50 34.25 Osaka
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 135.50 34.69 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-137.0 35.08 Nagoya
+-F+f12p,21,black+jLB -Gwhite@50 >> $ps << EOF
+137.0 34.93 Nagoya
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 136.9 35.18 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-141.35 43.16 Sapporo
+-F+f12p,21,black+jLB -Gwhite@50 >> $ps << EOF
+141.50 42.86 Sapporo
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 141.35 43.06 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
+-F+f12p,21,black+jLB -Gwhite@50 >> $ps << EOF
 130.4 33.72 Fukuoka
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 130.4 33.58 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-134.69 34.79 Kobe
+-F+f12p,21,black+jLB -Gwhite@50 >> $ps << EOF
+134.10 34.82 Kobe
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 135.19 34.69 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-139.7 35.62 Kawasaki
+-F+f11p,21,black+jLB -Gwhite@50 >> $ps << EOF
+139.9 35.32 Kawasaki
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 139.7 35.52 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
-135.77 35.16 Kyoto
+-F+f12p,21,black+jLB -Gwhite@50 >> $ps << EOF
+135.50 35.16 Kyoto
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 135.77 35.01 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,21,black+jLB -Gwhite@50 >> $ps << EOF
+-F+f11p,21,black+jLB -Gwhite@50 >> $ps << EOF
 139.64 36.01 Saitama
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 139.64 35.86 0.20c
 EOF
+gmt pstext -R -J -N -O -K \
+-F+f15p,0,black+jLB -Gwhite@50 >> $ps << EOF
+139.79 35.65 Tokyo
+EOF
+gmt psxy -R -J -Ss -W0.5p -Gred -O -K << EOF >> $ps
+139.69 35.69 0.30c
+EOF
 
-# insert global map (Countries codes: ISO 3166-1 alpha-2)
-gmt psbasemap -R -J -O -K -DjBR+w3.2c+o-0.2c/-0.2c+stmp >> $ps
+# insert global map (Countries codes: ISO 3166-1 alpha-2) w3.2c+o-0.2c/-0.2c
+gmt psbasemap -R -J -O -K -DjBR+w3.2c+stmp >> $ps
 read x0 y0 w h < tmp
-gmt pscoast --MAP_GRID_PEN_PRIMARY=thinnest,lightgray -Rg -JG140/37N/$w -Da -Gwheat3 -A2000 -Bga -Wfaint -EJP+gyellow -Sslategray3 -O -K -X$x0 -Y$y0 >> $ps
-#gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
+gmt pscoast --MAP_GRID_PEN_PRIMARY=thinnest,lightgray --MAP_FRAME_PEN=white -Rg -JG140/37N/$w -Da -Gpapayawhip -A2000 -Bga -Wfaint -EJP+gyellow -Ssteelblue4 -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
-# lightskyblue1
 
 # Add GMT logo
 gmt logo -Dx6.7/-1.8+o0.1i/0.1i+w2c -O -K >> $ps
