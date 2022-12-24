4c6c010c47e9b48b0364fdd90934a437f5add276
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2022-12-24 11:02:48 +0100

    Add files via upload

--- GMT-01-JP.sh
+++ GMT-01-JP.sh
@@ -14,7 +14,7 @@ gmt set FORMAT_GEO_MAP=dddF \
     MAP_TICK_PEN_PRIMARY=thinner,dimgray \
     MAP_GRID_PEN_PRIMARY=thin,white \
     MAP_GRID_PEN_SECONDARY=thinner,white \
-    FONT_TITLE=12p,Palatino-Roman,black \
+    FONT_TITLE=12p,0,black \
     FONT_ANNOT_PRIMARY=8p,Helvetica,black \
     FONT_LABEL=8p,Helvetica,black \
 # Overwrite defaults of GMTs
@@ -44,7 +44,7 @@ ps=Topo_JP.ps
 gmt grdimage jp_relief.nc -Cmyocean.cpt -R128/150/30/46 -JM16c -P -I+a15+ne0.75 -Xc -K > $ps
 
 # Add color legend
-gmt psscale -Dg124.5/30+w15.0c/0.4c+v+o0.3/0i+ml -R -J -Cmyocean.cpt \
+gmt psscale -Dg125.0/30+w15.0c/0.4c+v+o0.3/0i+ml -R -J -Cmyocean.cpt \
 	--FONT_LABEL=10p,0,black \
 	--FONT_ANNOT_PRIMARY=8p,0,black \
 	-Bg2000f100a1000+l"Topographic color scale" \
@@ -60,6 +60,7 @@ gmt pscoast -R -J -Ia/thinner,blue -Na -N1/thicker,red -W0.1p -Df -O -K >> $ps
 gmt psbasemap -R -J \
     --MAP_FRAME_AXES=wESN \
     --FORMAT_GEO_MAP=ddd:mm:ssF \
+    --FONT_TITLE=14p,0,black \
     -Bpxg8f2a4 -Bpyg6f3a3 -Bsxg4 -Bsyg3 \
     -B+t"Topographic map of Japan" -O -K >> $ps
     
@@ -74,7 +75,7 @@ gmt psbasemap -R -J \
 gmt psbasemap -R -J \
     --FONT=9p,Palatino-Roman,white \
     --MAP_TITLE_OFFSET=0.3c \
-    -Tdx14.5c/1.5c+w0.3i+f2+l+o0.15i \
+    -Tdx14.5c/9.5c+w0.3i+f2+l+o0.15i \
     -O -K >> $ps
     
 # Texts
@@ -94,14 +95,14 @@ gmt pstext -R -J -N -O -K \
 EOF
 
 # insert global map (Countries codes: ISO 3166-1 alpha-2)
-gmt psbasemap -R -J -O -K -DjBL+w3.2c+o-0.2c/-0.2c+stmp >> $ps
+gmt psbasemap -R -J -O -K -DjBR+w3.2c+o-0.2c/-0.2c+stmp >> $ps
 read x0 y0 w h < tmp
-gmt pscoast --MAP_GRID_PEN_PRIMARY=thinnest,grey -Rg -JG140/37N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -EJP+gred -Sdodgerblue -O -K -X$x0 -Y$y0 >> $ps
+gmt pscoast --MAP_GRID_PEN_PRIMARY=thinnest,grey -Rg -JG140/37N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -EJP+gred -Slightskyblue1 -O -K -X$x0 -Y$y0 >> $ps
 #gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
 # Add GMT logo
-gmt logo -Dx6.5/-2.2+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx6.7/-1.8+o0.1i/0.1i+w2c -O -K >> $ps
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y8.4c -N -O \
     -F+f10p,0,black+jLB >> $ps << EOF
