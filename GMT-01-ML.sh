24416a45f15df4902ee5322c0d9f9dcb8652728e
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-01-05 16:59:47 +0100

    Add files via upload

--- GMT-01-ML.sh
+++ GMT-01-ML.sh
@@ -20,23 +20,23 @@ gmt set FORMAT_GEO_MAP=dddF \
 gmtdefaults -D > .gmtdefaults
 
 # Extract a subset of ETOPO1m for the study area
-gmt grdcut ETOPO1_Ice_g_gmt4.grd -R-6/3/9/15.5 -Gml1_relief.nc
-gmt grdcut GEBCO_2019.nc -R-6/3/9/15.5 -Gml_relief.nc
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R-13/5/9.5/25.5 -Gml1_relief.nc
+gmt grdcut GEBCO_2019.nc -R-13/5/9.5/25.5 -Gml_relief.nc
 gdalinfo -stats ml1_relief.nc
-# Topography: Minimum=29.000, Maximum=896.000, Mean=288.667, StdDev=65.948
+# Topography: Minimum=27.000, Maximum=1590.000, Mean=327.682, StdDev=101.355
 
 # Make color palette
-gmt makecpt -Cearth -V -T29/896 > pauline.cpt
+gmt makecpt -Cgeo -V -T27/1590 > pauline.cpt
 # elevation etopo1 world elevation dem1 dem2 dem3 globe geo srtm turbo terra earth
 
 #####################################################################
 # create mask of vector layer from the DCW of country's polygon
-gmt pscoast -R-6/3/9/15.5 -JM5.5i -Dh -M -EML > Mali.txt
+gmt pscoast -R-13/5/9.5/25.5 -JM6.5i -Dh -M -EML > Mali.txt
 #####################################################################
 
-ps=Topo_BF.ps
+ps=Topo_ML.ps
 # Make background transparent image
-gmt grdimage ml_relief.nc -Cpauline.cpt -R-6/3/9/15.5 -JM5.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
+gmt grdimage ml_relief.nc -Cpauline.cpt -R-13/5/9.5/25.5 -JM6.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
     
 # Add isolines
 gmt grdcontour ml1_relief.nc -R -J -C1000 -A1000+f7p,26,darkbrown -Wthinner,darkbrown -O -K >> $ps
@@ -49,11 +49,11 @@ gmt pscoast -R -J \
 # CLIPPING
 # 1. Start: clip the map by mask to only include country
 
-gmt psclip -R-6/3/9/15.5 -JM5.5i BurkinaFaso.txt -O -K >> $ps
+gmt psclip -R-13/5/9.5/25.5 -JM6.5i Mali.txt -O -K >> $ps
 
 # 2. create map within mask
 # Add raster image
-gmt grdimage ml_relief.nc -Cpauline.cpt -R-6/3/9/15.5 -JM5.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
+gmt grdimage ml_relief.nc -Cpauline.cpt -R-13/5/9.5/25.5 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
 # Add isolines
 gmt grdcontour ml1_relief.nc -R -J -C500 -Wthinnest,darkbrown -O -K >> $ps
 # Add coastlines, borders, rivers
@@ -67,7 +67,7 @@ gmt psclip -C -O -K >> $ps
 #####################################################################
     
 # Add color legend
-gmt psscale -Dg-6.3/8.2+w14.0c/0.15i+h+o0.3/0i+ml+e -R -J -Cpauline.cpt \
+gmt psscale -Dg-13.3/9.0+w15.0c/0.15i+h+o0.3/0i+ml+e -R -J -Cpauline.cpt \
     --FONT_LABEL=8p,0,black \
     --FONT_ANNOT_PRIMARY=7p,0,black \
     --FONT_TITLE=6p,0,black \
@@ -91,18 +91,18 @@ gmt psbasemap -R -J \
     --FONT_ANNOT_PRIMARY=8p,0,black \
     --MAP_TITLE_OFFSET=0.1c \
     --MAP_ANNOT_OFFSET=0.1c \
-    -Lx11.7c/-2.4c+c10+w250k+l"Mercator projection. Scale (km)"+f \
+    -Lx11.7c/-2.4c+c10+w500k+l"Mercator projection. Scale (km)"+f \
     -UBL/0p/-70p -O -K >> $ps
 
 # Texts
-# Cities -R-6/3/9/15.5
+# Cities -R-13/5/9.5/25.5
 #
 # countries
 # water
 
 # insert map
 # Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
-gmt psbasemap -R -J -O -K -DjTR+w2.7c+o-0.2c/-0.2c+stmp >> $ps
+gmt psbasemap -R -J -O -K -DjTL+w3.5c+o-0.2c/-0.2c+stmp >> $ps
 read x0 y0 w h < tmp
 gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG-1.0/8.0N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -EML+gred -Sdodgerblue -O -K -X$x0 -Y$y0 >> $ps
 #gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
@@ -112,10 +112,10 @@ gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 gmt logo -Dx6.0/-3.1+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
-gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y4.0c -N -O \
+gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y8.0c -N -O \
     -F+f10p,0,black+jLB >> $ps << EOF
 01.5 10.4 Digital elevation data: SRTM/GEBCO, 15 arc sec resolution grid
 EOF
 
 # Convert to image file using GhostScript
-gmt psconvert Topo_BF.ps -A0.5c -E720 -Tj -Z
+gmt psconvert Topo_ML.ps -A0.5c -E720 -Tj -Z
