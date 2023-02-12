d3700bc345bff6381131347e1067d2e96b287cf8
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-02-12 15:01:54 +0100

    Add files via upload

--- GMT-01-CF.sh
+++ GMT-01-CF.sh
@@ -20,24 +20,24 @@ gmt set FORMAT_GEO_MAP=dddF \
 gmtdefaults -D > .gmtdefaults
 
 # Extract a subset of ETOPO1m for the study area
-gmt grdcut ETOPO1_Ice_g_gmt4.grd -R20/40/8/24.5 -Gcf1_relief.nc
-gmt grdcut GEBCO_2019.nc -R20/40/8/24.5 -Gcf_relief.nc
-gdalinfo -stats cf1_relief.nc
-# actual_range={-2756,4326}
+gmt grdcut ETOPO1_Ice_g_gmt4.grd -R14/28/2.5/11.5 -Gcf1_relief.nc
+gmt grdcut GEBCO_2019.nc -R14/28/2.5/11.5 -Gcf_relief.nc
+gdalinfo -stats cf_relief.nc
+# actual_range={212.1953125,1819.83984375}
 
 # Make color palette
-gmt makecpt -Cgeo -V -T-2756/3042 > pauline.cpt
+gmt makecpt -Cearth -V -T212/1820 > pauline.cpt
 # elevation etopo1 world elevation dem1 dem2 dem3 globe geo srtm turbo terra earth
 
 #####################################################################
 # create mask of vector layer from the DCW of country's polygon
-gmt pscoast -R20/40/8/24.5 -JM6.5i -Dh -M -ESD > CAR.txt
+gmt pscoast -R14/28/2.5/11.5 -JM6.5i -Dh -M -ECF > CAR.txt
 #####################################################################
 
 ps=Topo_CF.ps
 # Make background transparent image
 
-gmt grdimage cf_relief.nc -Cpauline.cpt -R20/40/8/24.5 -JM6.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
+gmt grdimage cf_relief.nc -Cpauline.cpt -R14/28/2.5/11.5 -JM6.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
 
 # Add isolines
 gmt grdcontour cf1_relief.nc -R -J -C250 -A500+f7p,26,darkbrown -Wthinnest,darkbrown -O -K >> $ps
@@ -50,11 +50,11 @@ gmt pscoast -R -J \
 # CLIPPING
 # 1. Start: clip the map by mask to only include country
 
-gmt psclip -R20/40/8/24.5 -JM6.5i CAR.txt -O -K >> $ps
+gmt psclip -R14/28/2.5/11.5 -JM6.5i CAR.txt -O -K >> $ps
 
 # 2. create map within mask
 # Add raster image
-gmt grdimage cf_relief.nc -Cpauline.cpt -R20/40/8/24.5 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
+gmt grdimage cf_relief.nc -Cpauline.cpt -R14/28/2.5/11.5 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
 # Add isolines
 gmt grdcontour cf1_relief.nc -R -J -C250 -A500+f7p,26,darkbrown -Wthinnest,darkbrown -O -K >> $ps
 # Add coastlines, borders, rivers
@@ -68,11 +68,11 @@ gmt psclip -C -O -K >> $ps
 #####################################################################
     
 # Add color legend
-gmt psscale -Dg19.5/6.5+w16.5c/0.15i+h+o0.3/0i+ml+e -R -J -Cpauline.cpt \
+gmt psscale -Dg14/1.5+w16.0c/0.15i+h+o0.3/0i+ml+e -R -J -Cpauline.cpt \
     --FONT_LABEL=8p,0,black \
     --FONT_ANNOT_PRIMARY=7p,0,black \
     --FONT_TITLE=6p,0,black \
-    -Bg500f100a500+l"Colormap: 'geo' Colors for global topography relief [R=-2756/3042, H, C=RGB]" \
+    -Bg200f10a200+l"Colormap: 'geo' Colors for global topography relief [R=-2756/3042, H, C=RGB]" \
     -I0.2 -By+lm -O -K >> $ps
     
 # Add grid
@@ -92,24 +92,24 @@ gmt psbasemap -R -J \
     --FONT_ANNOT_PRIMARY=9p,0,black \
     --MAP_TITLE_OFFSET=0.1c \
     --MAP_ANNOT_OFFSET=0.1c \
-    -Lx13.0c/-2.4c+c10+w500k+l"Mercator projection. Scale (km)"+f \
+    -Lx14.5c/-2.4c+c10+w300k+l"Mercator projection. Scale (km)"+f \
     -UBL/0p/-70p -O -K >> $ps
 
-# Cities -R20/40/8/24.5
+# Cities -R14/28/2.5/11.5
 
 # insert map
 # Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
-gmt psbasemap -R -J -O -K -DjBR+w3.5c+o-0.2c/-0.2c+stmp >> $ps
+gmt psbasemap -R -J -O -K -DjTL+w3.5c+o-0.2c/-0.2c+stmp >> $ps
 read x0 y0 w h < tmp
 gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG28.0/8.0N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -ECF+gred -Sdodgerblue -O -K -X$x0 -Y$y0 >> $ps
 #gmt pscoast -Rg -JG12/5N/$w -Da -Gbrown -A5000 -Bg -Wfaint -ECM+gbisque -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
 # Add GMT logo
-gmt logo -Dx6.5/-3.1+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx7.0/-3.1+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
-gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y7.6c -N -O \
+gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y4.5c -N -O \
     -F+f10p,0,black+jLB >> $ps << EOF
 3.0 10.4 Digital elevation data: SRTM/GEBCO, 15 arc sec resolution grid
 EOF
