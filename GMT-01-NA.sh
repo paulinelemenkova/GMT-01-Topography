27c56bd166dd96b99579caeb0f758c4e03ad2e28
Polina Lemenkova <pauline.lemenkova@gmail.com>
2023-12-24 22:24:12 +0100

    Add files via upload

--- GMT-01-NA.sh
+++ GMT-01-NA.sh
@@ -31,14 +31,14 @@ gmt pscoast -R10/26/-30/-16 -JM6.5i -Dh -M -ENA > Namibia.txt
 
 ps=Topo_NA.ps
 # Make background transparent image
-gmt grdimage na1_relief.nc -Cpauline.cpt -R10/26/-30/-16 -JM6.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
+gmt grdimage na_relief.nc -Cpauline.cpt -R10/26/-30/-16 -JM6.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
 
 # Add isolines
 gmt grdcontour na1_relief.nc -R -J -C500 -A1000+f7p,26,darkbrown -Wthinner,darkbrown -O -K >> $ps
 
 # Add coastlines, borders, rivers
 gmt pscoast -R -J \
-    -Ia/thinner,blue -Na -N1/thicker,tomato -W0.1p -Df -O -K >> $ps
+    -Ia/thinner,blue -Na -N1/thickest,tomato -W0.1p -Df -O -K >> $ps
 # Add lakes
 # gmt pscoast -R -J -Ia/thinner,blue -Na -Sroyalblue1 -W2/thin,blue,0.1p -Df -O -K >> $ps
     
@@ -49,12 +49,12 @@ gmt psclip -R10/26/-30/-16 -JM6.5i Namibia.txt -O -K >> $ps
 
 # 2. create map within mask
 # Add raster image
-gmt grdimage na1_relief.nc -Cpauline.cpt -R10/26/-30/-16 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
+gmt grdimage na_relief.nc -Cpauline.cpt -R10/26/-30/-16 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
 # Add isolines
 gmt grdcontour na1_relief.nc -R -J -C500 -A1000+f7p,26,darkbrown -Wthinnest,darkbrown -O -K >> $ps
 # Add coastlines, borders, rivers
 gmt pscoast -R -J \
-    -Ia/thinner,blue -Na -N1/thicker,tomato -W0.1p -Df -O -K >> $ps
+    -Ia/thinner,blue -Na -N1/thickest,tomato -W0.1p -Df -O -K >> $ps
 # add lakes
 gmt pscoast -R -J -Ia/thinner,blue -Na -Sroyalblue1 -W2/thin,blue,0.1p -Df -O -K >> $ps
 
@@ -89,11 +89,14 @@ gmt psbasemap -R -J \
     --FONT_ANNOT_PRIMARY=9p,0,black \
     --MAP_TITLE_OFFSET=0.1c \
     --MAP_ANNOT_OFFSET=0.1c \
-    -Lx14.0c/-1.3c+c10+w400k+l"Mercator projection. Scale (km)"+f \
+    -Lx14.0c/-1.0c+c10+w400k+l"Mercator projection. Scale (km)"+f \
     -UBL/0p/-30p -O -K >> $ps
 
 # Study area
 # Rotated rectangle. kwargs: -Sjdirection/width/height, coords
+gmt psxy -R -J -Sj-13/2.0/2.0 -W2.0p,cyan1 -O -K << EOF >> $ps
+16.30 -18.70
+EOF
 
 # Texts
 gmt pstext -R -J -N -O -K \
@@ -106,11 +109,11 @@ gmt pstext -R -J -N -O -K \
 EOF
 gmt pstext -R -J -N -O -K \
 -F+f14p,0,ivory+jLB >> $ps << EOF
-20.3 -23.5 B O T S W A N A
+20.3 -23.7 B O T S W A N A
 EOF
 gmt pstext -R -J -N -O -K \
 -F+f14p,0,ivory+jLB >> $ps << EOF
-21.0 -27.7 S O U T H  A F R I C A
+20.50 -27.6 S O U T H   A F R I C A
 EOF
 gmt pstext -R -J -N -O -K \
 -F+f14p,23,white+jLB >> $ps << EOF
@@ -118,6 +121,49 @@ gmt pstext -R -J -N -O -K \
 10.2 -26.5 O C E A N
 EOF
 # rivers
+gmt pstext -R -J -N -O -K \
+-F+f13p,23,blue+jLB+a43 >> $ps << EOF
+17.60 -21.3 Omatako
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f11p,23,midnightblue+jLB+a20 >> $ps << EOF
+16.05 -18.9 Etosha
+16.25 -19.2 Pan
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f13p,23,blue+jLB+a-5 >> $ps << EOF
+18.80 -17.7 Okavango
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f12p,23,blue+jLB+a-40 >> $ps << EOF
+19.10 -24.0 Nossob
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f12p,23,blue+jLB+a75 -Gpalegoldenrod@40 >> $ps << EOF
+18.05 -27.4 Fish
+EOF
+# geography
+gmt pstext -R -J -N -O -K \
+-F+f14p,2,darkred+jLB >> $ps << EOF
+20.3 -24.4 K A L A H A R I
+20.3 -24.9 D E S E R T
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f12p,23,darkred+jLB+a-60 -Gpalegoldenrod@40 >> $ps << EOF
+13.65 -20.90 N A M I B
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f12p,23,darkred+jLB+a-80 -Gpalegoldenrod@40 >> $ps << EOF
+14.80 -23.70 D E S E R T
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f12p,2,lemonchiffon+jLB+a10 >> $ps << EOF
+16.60 -19.70 Otavi Mts.
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f12p,23,darkolivegreen+jLB+a-60 -Ghoneydew@40 >> $ps << EOF
+11.95 -17.80 Skeleton Coast Park
+EOF
 # cities
 gmt pstext -R -J -N -O -K \
 -F+f13p,0,mintcream+jLB >> $ps << EOF
@@ -142,7 +188,7 @@ gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 EOF
 gmt pstext -R -J -N -O -K \
 -F+f13p,0,mintcream+jLB >> $ps << EOF
-16.75 -20.75 Otjiwarongo
+16.10 -20.15 Otjiwarongo
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 16.65 -20.45 0.20c
@@ -169,7 +215,7 @@ gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
 14.53 -22.68 0.20c
 EOF
 gmt pstext -R -J -N -O -K \
--F+f13p,0,mintcream+jLB >> $ps << EOF
+-F+f13p,0,mintcream+jLB -Ggoldenrod@50 >> $ps << EOF
 14.6 -22.95 Walvis Bay
 EOF
 gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
@@ -192,7 +238,7 @@ gmt pscoast --MAP_GRID_PEN_PRIMARY=thinnest,lightgray --MAP_FRAME_PEN=thick,whit
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
 # Add GMT logo
-gmt logo -Dx7.0/-1.8+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx7.0/-1.5+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y9.3c -N -O \
