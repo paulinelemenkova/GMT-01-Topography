6061630c491a6b3191236d5caa060a1ab4800ced
Polina Lemenkova <pauline.lemenkova@gmail.com>
2024-01-12 19:21:34 +0100

    Add files via upload

--- GMT-01-CN_JU.sh
+++ GMT-01-CN_JU.sh
@@ -70,7 +70,7 @@ gmt psscale -Dg70/8.5+w16.0c/0.15i+h+o0.3/0i+ml+e -R -J -Cpauline.cpt \
     --FONT_LABEL=8p,0,black \
     --FONT_ANNOT_PRIMARY=8p,0,black \
     --FONT_TITLE=8p,0,black \
-    -Bg2000f100a2000+l"Colormap: 'geo' Colors for global bathymetry/topography relief [R=-4373/3703, H, C=RGB]" \
+    -Bg2000f100a2000+l"Colormap: 'geo' [R=-7795/8271, H, C=RGB]; transparence des régions voisines : 70%" \
     -I0.2 -By+lm -O -K >> $ps
     
 # Add grid
@@ -79,12 +79,12 @@ gmt psbasemap -R -J \
     --FORMAT_GEO_MAP=ddd:mm:ssF \
     --MAP_TICK_PEN_PRIMARY=thinner,dimgray \
     --MAP_FRAME_WIDTH=0.1c \
-    --MAP_TITLE_OFFSET=0.1c \
+    --MAP_TITLE_OFFSET=-0.3c \
     --FONT_ANNOT_PRIMARY=8p,0,black \
     --FONT_LABEL=8p,25,black \
     --FONT_TITLE=12p,0,black \
     -Bpxf5a10g5 -Bpyg5f5a10 -Bsxg5 -Bsyg5 \
-    -B+t"Carte topographique du Chine" -O -K >> $ps
+    -B+t"Carte générale de la Chine" -O -K >> $ps
     
 # Add scalebar, directional rose
 gmt psbasemap -R -J \
@@ -95,21 +95,43 @@ gmt psbasemap -R -J \
     -Lx14.5c/-2.5c+c10+w2000k+l"Projection UTM, Zone 47.5, échelle (km)"+f \
     -UBL/0p/-70p -O -K >> $ps
 
+# Study area
+# Rotated rectangle. kwargs: -Sjdirection/width/height, coords
+# LAT: 34°36'38.05"N LON: 5°32'33.07"W
+gmt psxy -R -J -Sj-13/0.5/0.5 -W1.0p,yellow -O -K << EOF >> $ps
+114.00 30.30
+EOF
+
 # Texts
+# Cities -R-15/0/27/37
+gmt pstext -R -J -N -O -K \
+-F+f9p,0,ivory+jLB >> $ps << EOF
+114.85 31.6 Wuhan
+EOF
+gmt psxy -R -J -Sc -W0.5p -Gyellow -O -K << EOF >> $ps
+114.30 30.59 0.15c
+EOF
+gmt pstext -R -J -N -O -K \
+-F+f10p,0,ivory+jLB >> $ps << EOF
+111.70 41.30 Beijing
+EOF
+gmt psxy -R -J -Sa -W0.5p,red -Gred -O -K << EOF >> $ps
+116.39 39.90 0.35c
+EOF
 
 # insert map
 gmt psbasemap -R -J -O -K -DjTL+w3.2c+stmp >> $ps
 read x0 y0 w h < tmp
-gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey --MAP_FRAME_PEN=thin,azure -Rg -JG100/30N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -ECN+gred -Sroyalblue3 -O -K -X$x0 -Y$y0 >> $ps
+gmt pscoast --MAP_GRID_PEN_PRIMARY=thinner,white --MAP_FRAME_PEN=thin,azure -Rg -JG100/30N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -ECN+gred -Sroyalblue3 -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
 # Add GMT logo
 gmt logo -Dx7.0/-3.1+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
-gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y6.3c -N -O \
-    -F+f10p,0,black+jLB >> $ps << EOF
-5.5 7.0 et zone d'études (Wuhan)
+gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y6.0c -N -O \
+    -F+f9p,0,black+jLB >> $ps << EOF
+6.0 7.0 et zone d'études (Wuhan)
 EOF
 
 # Convert to image file using GhostScript
