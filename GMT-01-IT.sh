60a1f7800aec05c625973b6004ecce5b831cae3b
Polina Lemenkova <pauline.lemenkova@gmail.com>
2024-06-03 11:23:03 +0200

    Add files via upload

--- GMT-01-IT.sh
+++ GMT-01-IT.sh
@@ -24,7 +24,6 @@ gmt grdcut ETOPO1_Ice_g_gmt4.grd -R6/20/36/48 -Git1_relief.nc
 gmt grdcut GEBCO_2023.nc -R6/20/36/48 -Git_relief.nc
 gmt grdinfo -M it_relief.nc
 # gdalinfo -stats it_relief.nc
-# Topography: actual_range={-3746/1398}
 
 # Make color palette
 gmt makecpt -Cgeo -V -T-4151/4655 > pauline.cpt
@@ -32,12 +31,12 @@ gmt makecpt -Cgeo -V -T-4151/4655 > pauline.cpt
 
 #####################################################################
 # create mask of vector layer from the DCW of country's polygon
-gmt pscoast -R6/20/36/48 -JM5.5i -Dh -M -EIT > Italy.txt
+gmt pscoast -R6/20/36/48 -JM6.5i -Dh -M -EIT > Italy.txt
 #####################################################################
 
 ps=Topo_IT.ps
 # Make background transparent image
-gmt grdimage it_relief.nc -Cpauline.cpt -R6/20/36/48 -JM5.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
+gmt grdimage it1_relief.nc -Cpauline.cpt -R6/20/36/48 -JM6.5i -I+a15+ne0.75 -t40 -Xc -P -K > $ps
     
 # Add isolines
 gmt grdcontour it1_relief.nc -R -J -C500 -A1000+f7p,26,darkbrown -Wthinner,darkbrown -O -K >> $ps
@@ -46,31 +45,28 @@ gmt grdcontour it1_relief.nc -R -J -C500 -A1000+f7p,26,darkbrown -Wthinner,darkb
 gmt pscoast -R -J \
     -Ia/thinner,blue -Na -N1/thick,dimgray -W0.1p -Df -O -K >> $ps
 gmt pscoast -R -J \
-    -Ia/thinner,blue -Na -N1/thickest,tomato -W0.1p -Df -O -K >> $ps
+    -Ia/thinner,blue -Na -N1/thicker,tomato -W0.1p -Df -O -K >> $ps
     
 #####################################################################
 # CLIPPING
 # 1. Start: clip the map by mask to only include country
-
-gmt psclip -R6/20/36/48 -JM5.5i Italy.txt -O -K >> $ps
+gmt psclip -R6/20/36/48 -JM6.5i Italy.txt -O -K >> $ps
 
 # 2. create map within mask
 # Add raster image
-gmt grdimage it_relief.nc -Cpauline.cpt -R6/20/36/48 -JM5.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
+gmt grdimage it1_relief.nc -Cpauline.cpt -R6/20/36/48 -JM6.5i -I+a15+ne0.75 -Xc -P -O -K >> $ps
 # Add isolines
 gmt grdcontour it1_relief.nc -R -J -C500 -Wthinnest,darkbrown -O -K >> $ps
 # Add coastlines, borders, rivers
 gmt pscoast -R -J \
-    -Ia/thinner,blue -Na -N1/thickest,tomato -W0.1p -Df -O -K >> $ps
-#gmt pscoast -R -J \
-    -Ia/thinner,blue -Na -W0.1p -Df -O -K >> $ps
+    -Ia/thinner,blue -Na -N1/thicker,tomato -W0.1p -Df -O -K >> $ps
 
 # 3: Undo the clipping
 gmt psclip -C -O -K >> $ps
 #####################################################################
     
 # Add color legend
-gmt psscale -Dg-0.9/5.5+w19.0c/0.15i+v+o0.3/0i+ml+e -R -J -Cpauline.cpt \
+gmt psscale -Dg4/36+w19.0c/0.15i+v+o0.3/0i+ml+e -R -J -Cpauline.cpt \
     --FONT_LABEL=10p,0,black \
     --FONT_ANNOT_PRIMARY=10p,0,black \
     --FONT_TITLE=10p,0,black \
@@ -86,36 +82,68 @@ gmt psbasemap -R -J \
     --FONT_LABEL=10p,25,black \
     --FONT_TITLE=13p,0,black \
         -Bpxg2f1a1 -Bpyg2f1a1 -Bsxg2 -Bsyg1 \
-    -B+t"Stidy area on the topographic map of Italy" -O -K >> $ps
+    -B+t"Study area within the topographic map of Italy" -O -K >> $ps
     
 # Add scalebar, directional rose
 gmt psbasemap -R -J \
     --FONT_LABEL=12p,0,black \
-    --FONT_ANNOT_PRIMARY=12p,0,black \
+    --FONT_ANNOT_PRIMARY=11p,0,black \
     --MAP_TITLE_OFFSET=0.1c \
     --MAP_ANNOT_OFFSET=0.1c \
-    -Lx11.7c/-1.7c+c10+w200k+l"Mercator projection. Scale (km)"+f \
+    -Lx13.5c/-1.7c+c10+w400k+l"Mercator projection. Scale (km)"+f \
     -UBL/0p/-40p -O -K >> $ps
-
+    
+# Study area
+# Rotated rectangle. kwargs: -Sjdirection/width/height, coords
+# LAT: 43.8675 LON: 11.779444
+gmt psxy -R -J -Sj0/0.7/0.7 -W2.0p,yellow -O -K << EOF >> $ps
+11.78 43.87
+EOF
 # Texts
-
-#
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,blue+jLB+a-45 >> $ps << EOF
+13.3 44.2 A d r i a t i c   S e a
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,white+jLB >> $ps << EOF
+10.1 39.1 Tyrrhenian Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,white+jLB >> $ps << EOF
+8.1 43.7 Ligurian
+8.4 43.4 Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,white+jLB >> $ps << EOF
+17.1 38.1 Ionian
+17.5 37.8 Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f13p,26,white+jLB >> $ps << EOF
+6.1 38.1 Mediterranean
+6.5 37.7 Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f12p,26,blue+jLB >> $ps << EOF
+12.5 45.1 Gulf
+12.5 44.7 of Venice
+EOF
 
 # insert map
 # Countries codes: ISO 3166-1 alpha-2. Continent codes AF (Africa), AN (Antarctica), AS (Asia), EU (Europe), OC (Oceania), NA (North America), or SA (South America). -EEU+ggrey
-gmt psbasemap -R -J -O -K -DjBR+w4.5c+o-0.2c/-0.2c+stmp >> $ps
+gmt psbasemap -R -J -O -K -DjTR+w3.5c+o-0.2c/-0.2c+stmp >> $ps
 read x0 y0 w h < tmp
 gmt pscoast --MAP_GRID_PEN_PRIMARY=thin,grey -Rg -JG12/40.0N/$w -Da -Glightgoldenrod1 -A5000 -Bga -Wfaint -EIT+gred -Sdodgerblue -O -K -X$x0 -Y$y0 >> $ps
 gmt psxy -R -J -O -K -T  -X-${x0} -Y-${y0} >> $ps
 
 # Add GMT logo
-gmt logo -Dx5.5/-2.0+o0.1i/0.1i+w2c -O -K >> $ps
+gmt logo -Dx6.5/-2.1+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
-gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y14.0c -N -O \
+gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y13.2c -N -O \
     -F+f12p,0,black+jLB >> $ps << EOF
-0.0 10.0 Data : GEBCO, resolution: 15 arc sec
+4.0 10.0 Data: GEBCO grid, resolution: 15 arc sec
 EOF
 
 # Convert to image file using GhostScript
-gmt psconvert Topo_IT.ps -A0.5c -E720 -Tj -Z
+gmt psconvert Topo_IT.ps -A1.5c -E720 -Tj -Z
